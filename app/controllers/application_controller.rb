9f25717d761a7ddc96c9aa47c490aad45f1c2fae
Michael Mahaffey <mayhaffs@gmail.com>
2018-10-10 21:46:54 -0500

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+	def hello
+		render html: "hola, mundo!"
+	end
+
+	def goodbye
+		render html: "goodbye, world!"
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
